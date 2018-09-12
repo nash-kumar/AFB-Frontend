1d8f2fa4ed5fa8de4c6d28a28cb078bf0fd30236
Vinay-Sunny <vinaysunny9@accionlabs.com>
2018-09-12 16:33:16 +0530

    vinay_registerPage

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  fname:String='';
+  lname:String='';
+  email;
+  phone:Number;
+  date;
+  pass;
+  constructor(private fb:FormBuilder) { 
+
+  
+    this.registerForm = fb.group({
+      "fname": [null, Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]+$')])],
+      "lname": [null, Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]+$')])],
+      "email": [null, Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9._]+$')])],
+      "phone": [null, Validators.compose([Validators.required, Validators.pattern('^[0-9]{10}$')])],
+      "date": [null , Validators.required],
+      "pass": [null,Validators.required]
+    })
+  
+  }
+  ngOnInit(){}
+}
